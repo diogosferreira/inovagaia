ba0448fa32e74a4b319ab1128e2f2cdcd1bbfd9a
diogosferreira <diogosferreira@users.noreply.github.com>
2025-09-30 17:21:42 +0100

    sned

--- public/code.js
+++ public/code.js
@@ -3,6 +3,7 @@ import { marqueeSpeed } from './marquee/marquee-speed.js';
 import { copyright } from './copyright/copyright.js';
 import { swiperMentors } from './swiper/swiper-mentors.js';
 import { swiperSessions } from './swiper/swiper-sessions.js';
+import { swiperTestimonials } from './swiper/swiper-testimonials.js';
 import { timeline } from './timeline/timeline.js';
 
 
@@ -14,6 +15,7 @@ function initAll() {
     lenisAllowScroll();
     copyright();
     swiperSessions();
+    swiperTestimonials();
     timeline();
 }
 
