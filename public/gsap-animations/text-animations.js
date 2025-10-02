e8f92a65c8e3e1b48fae4563b3b317c6188ef50e
diogosferreira <diogosferreira@users.noreply.github.com>
2025-10-02 14:59:51 +0100

    send

--- public/gsap-animations/text-animations.js
+++ public/gsap-animations/text-animations.js
@@ -71,9 +71,7 @@ export function gsapTitles() {
                     invalidateOnRefresh: true,
                     // markers: true,
                 },
-                onComplete() {
-                    lines.forEach((ln) => (ln.style.willChange = ""));
-                },
+
             }
         );
     });
